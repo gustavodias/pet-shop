950c700b5a1dfd33a2477400b33d80e30226e988
gustavodias <gustavoemersonfd@gmail.com>
2022-04-03 14:37:38 -0300

    First commit

---
+++ back/src/main/java/com/petshop/PetShopAlpha/PetShopAlphaApplication.java
@@ -0,0 +1,13 @@
+package com.petshop.PetShopAlpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShopAlphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetShopAlphaApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/petshop/PetShopAlpha/PetShopAlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petshop.PetShopAlpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShopAlphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
