d981db1e5fd4138dc8f8a98ec912349f56863234
gustavo.emerson_meta <gustavo.emerson_meta>
2022-05-23 18:21:03 -0300

    Update PetShopAlphaApplication.java

--- back/src/main/java/com/petshop/PetShopAlpha/PetShopAlphaApplication.java
+++ back/src/main/java/com/petshop/PetShopAlpha/PetShopAlphaApplication.java
@@ -2,12 +2,21 @@ package com.petshop.PetShopAlpha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class PetShopAlphaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetShopAlphaApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder getPasswordEncoder(){
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
+	}
 }
